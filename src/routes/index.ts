f65ba4bfd9e8b94c0f30441fc014fc4c233ee751
Anderson1703 <adamesgithub@gmail.com>
2024-07-11 16:34:03 -0400

    in progress

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response, Router } from 'express';
 import AuthRouter from './auth.route';
 import ProductsRouter from './products.route';
+import UsersRouter from './users.route';
 
 const root = Router();
 const server = express();
@@ -12,6 +13,6 @@ root.get("/", (req: Request, res: Response) => {
 server.use("/", root);
 server.use("/products", ProductsRouter);
 server.use("/auth", AuthRouter);
-server.use("/users", AuthRouter);
+server.use("/users", UsersRouter);
 
 export default server;
